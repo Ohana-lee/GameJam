35a4ce2c7d6081be11031a02d00c65f817ae930f
Ohana-lee <uv20465@bristol.ac.uk>
2023-10-30 21:57:04 +0000

    fixed aspect ratio problems

--- The debugger/Assets/Scripts/SceneManagers.cs
+++ The debugger/Assets/Scripts/SceneManagers.cs
@@ -5,9 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagers : MonoBehaviour
 {
+    public bool cutscene;
+
     // Start is called before the first frame update
    public void Loadscene(string sceneName)
    {
         SceneManager.LoadScene(sceneName);
    }
+
+    void Update()
+    {
+        if (cutscene)
+        {
+            if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return)) //maybe change to enter button later
+            {
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+    }
 }
